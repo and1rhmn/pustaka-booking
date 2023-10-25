55db7fc3a741c57a1533ce33614cffceffb04158
and1rhmn <andi2504003@gmail.com>
2023-10-25 11:45:57 +0700

    menambahkan kalimat

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -6,9 +6,9 @@ class contoh1 extends CI_Controller
         echo "<h1> perkenalan </h1>";
         echo "Nama saya Andi Rahman Alamsyah
         saya tinggal di daerah cengkareng 
-        olahraga yang saya sukai adalah kamu ";
+        olahraga yang saya sukai adalah kamu wkwk";
 
-        echo "saya mau jadi kelas bintang";
+        echo "saya mau jadi chef juna hahuekk";
     }
 }
 
